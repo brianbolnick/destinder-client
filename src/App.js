70b856e9f683ee782b77080e1727fe8b0818e2ea
Brian Bolnick <bbolnick@instructure.com>
2017-09-28 17:37:37 -0600

    skeleton starts for homepage and mobile icon configs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Container, Header } from 'semantic-ui-react'
+import { Container, Sidebar, Segment, Button, Menu, Image, Icon, Header  } from 'semantic-ui-react'
 import './App.css';
+
+class SidebarLeftPush extends Component {
+  state = { visible: false }
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
+  render() {
+    const { visible } = this.state
+    return (
+      <div>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar as={Menu} animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>
+            <Menu.Item name='home'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item name='gamepad'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item name='camera'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Segment basic>
+              <Container fluid>
+                <Button floated='left' onClick={this.toggleVisibility} circular icon='sidebar' className="menu-icon"/>
+                <Header as='h2'>Welcome to Destinder! </Header>
+                <p>There's not much here yet, check back soon!</p>
+              </Container>
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+
+      
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-            <Container text>
-              <Header as='h2'>Welcome to Destinder! </Header>
-              <p>There's not much here yet, check back soon!</p>
-          </Container>
+          <SidebarLeftPush />
       </div>
     );
   }
