1a650ad5dba430ad5af828ac0e50843a74ba4f95
Brian Bolnick <bbolnick@instructure.com>
2017-09-29 00:13:39 -0600

    starts for sidebar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Container, Sidebar, Segment, Button, Menu, Image, Icon, Header  } from 'semantic-ui-react'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const Home = () => (
+  <div>
+    <Header as='h2'>Welcome to Destinder! </Header>
+    <p>There's not much here yet, check back soon!</p>
+  </div>
+)
+const Fireteams = () => (
+  <div>
+    <Header as='h2'>LFG!</Header>
+    <p>This is where all of the posts will be.</p>
+  </div>
+)
+
 
 class SidebarLeftPush extends Component {
   state = { visible: false }
@@ -11,35 +30,35 @@ class SidebarLeftPush extends Component {
   render() {
     const { visible } = this.state
     return (
-      <div>
-        <Sidebar.Pushable as={Segment}>
-          <Sidebar as={Menu} animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>
-            <Menu.Item name='home'>
-              <Icon name='home' />
-              Home
-            </Menu.Item>
-            <Menu.Item name='gamepad'>
-              <Icon name='gamepad' />
-              Games
-            </Menu.Item>
-            <Menu.Item name='camera'>
-              <Icon name='camera' />
-              Channels
-            </Menu.Item>
-          </Sidebar>
-          <Sidebar.Pusher>
-            <Segment basic>
-              <Container fluid>
-                <Button floated='left' onClick={this.toggleVisibility} circular icon='sidebar' className="menu-icon"/>
-                <Header as='h2'>Welcome to Destinder! </Header>
-                <p>There's not much here yet, check back soon!</p>
-              </Container>
-            </Segment>
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-
-      
-      </div>
+      <Router>
+        <div>
+          <Sidebar.Pushable as={Segment}>
+            <Sidebar as={Menu} animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>
+              <Menu.Item as={Link} to='/' name='home'>                
+                <Icon name='home' />
+                Home                
+              </Menu.Item>
+              <Menu.Item as={Link} to='/fireteams' name='gamepad'>
+                  <Icon name='gamepad' />
+                  LFG
+              </Menu.Item>
+              <Menu.Item name='camera'>
+                <Icon name='camera' />
+                Channels
+              </Menu.Item>
+            </Sidebar>
+            <Sidebar.Pusher>
+              <Segment basic>
+                <Container fluid>
+                  <Button floated='left' onClick={this.toggleVisibility} circular icon='sidebar' className="menu-icon"/>
+                  <Route exact path="/" component={Home}/>
+                  <Route path="/fireteams" component={Fireteams}/>
+                </Container>
+              </Segment>
+            </Sidebar.Pusher>
+          </Sidebar.Pushable>
+        </div>
+      </Router>
     )
   }
 }
