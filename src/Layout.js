8792b7a3eb84d5ff3cb27970ba13e2f2e8ddf142
Brian Bolnick <bbolnick@instructure.com>
2017-10-01 02:38:28 -0600

    update page structuring

---
+++ src/Layout.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Header from './Header.js'
+import { Grid, Menu, Segment, Label } from 'semantic-ui-react'
+
+const containerStyle = {
+    marginTop: '2%'
+  }
+
+const Layout = ({ children }) => (
+    
+
+    <Grid>
+        <Grid.Column width={2}>
+            <Header />
+        </Grid.Column>
+
+        <Grid.Column stretched width={14} style={containerStyle}>
+            {children}
+        </Grid.Column>
+    </Grid>
+)
+
+export default Layout
