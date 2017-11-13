8d891506616428f7fc75e14cb03de872d8ff689e
Brian Bolnick <brianbolnick@gmail.com>
2017-11-13 13:20:02 -0700

    add redux wrapper

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
-);
+ReactDOM.render((
+  <Provider store={store}>
+      <ConnectedRouter history={history}>
+          <div>
+              <App />
+          </div>
+      </ConnectedRouter>
+  </Provider>
+), document.getElementById('root'))
 registerServiceWorker();
