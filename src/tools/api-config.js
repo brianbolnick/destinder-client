a06ed20a3b6d5bc473a50933afe0e7aede32d933
Brian Bolnick <brianbolnick@gmail.com>
2018-01-05 11:02:17 -0700

    update api url for prod testing

--- src/tools/api-config.js
+++ src/tools/api-config.js
@@ -6,7 +6,8 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === 'localhost') {
   backendHost = 'http://localhost:5000';
 } else {
-  backendHost = 'https://destinder-api.herokuapp.com';
+  // backendHost = 'https://destinder-api.herokuapp.com';
+  backendHost = 'http://production.yvrgehncpb.us-east-1.elasticbeanstalk.com';
 }
 
 export const API_URL = `${backendHost}`;
