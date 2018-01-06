9afc26415afda74116cd2a6a53c8980691b5ebcb
Brian Bolnick <brianbolnick@gmail.com>
2018-01-06 11:12:56 -0700

    update admin page to display count without pulling all users right away

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Header, Table, Label } from "semantic-ui-react";
+import { Container, Header, Table, Label, Accordion, Icon } from "semantic-ui-react";
 import Layout from "./Layout.js";
 import axios from 'axios';
 import { BADGES } from '../data/common_constants'
@@ -9,7 +9,7 @@ class Admin extends Component {
 
     state = { users: [] }
 
-    componentWillMount() {
+    getAllUsers() {
         axios.get(`${API_URL}/v1/users`).then(response => {
 
             const users = response.data.map((user) => {
@@ -33,26 +33,73 @@ class Admin extends Component {
         }).catch(error => console.log(error))
     }
 
+    componentWillMount() {
+        axios.get(`${API_URL}/v1/users/total_count`).then(response => {
+            this.setState({ count: response.data.count })
+        }).catch(error => console.log(error))
+    }
+
+
+    handleClick = (e, titleProps) => {
+        const { index } = titleProps
+        const { activeIndex } = this.state
+        const newIndex = activeIndex === index ? -1 : index
+        axios.get(`${API_URL}/v1/users`).then(response => {
+
+            const users = response.data.map((user) => {
+                return (
+                    <Table.Row key={user.id}>
+                        <Table.Cell>{user.id}</Table.Cell>
+                        <Table.Cell>{user.display_name}</Table.Cell>
+                        <Table.Cell>{user.api_membership_type}</Table.Cell>
+                        <Table.Cell>{user.locale}</Table.Cell>
+                        <Table.Cell>
+                            <Label.Group>
+                                {user.badges.map((badge) => {
+                                    return BADGES[badge.id]
+                                })}
+                            </Label.Group>
+                        </Table.Cell>
+                    </Table.Row>
+                )
+            })
+            this.setState({ users: users })
+        }).catch(error => console.log(error))
+
+        this.setState({ activeIndex: newIndex })
+    }
+    
     render() {
+        const { activeIndex } = this.state
         return (
             <Layout>
                 <div className="profile-page" style={{ minHeight: '100%' }}>
                     <Container>
-                        <Header as="h2">Site Users (Total: {this.state.users.length})</Header>
-                        <Table celled>
-                            <Table.Header>
-                                <Table.Row>
-                                    <Table.HeaderCell>ID</Table.HeaderCell>
-                                    <Table.HeaderCell>User Name</Table.HeaderCell>
-                                    <Table.HeaderCell>Platform</Table.HeaderCell>
-                                    <Table.HeaderCell>Locale</Table.HeaderCell>
-                                    <Table.HeaderCell>Badges</Table.HeaderCell>
-                                </Table.Row>
-                            </Table.Header>
-                            <Table.Body>
-                                {this.state.users}
-                            </Table.Body>
-                        </Table>
+                        <Header as="h2">Site Users (Total: {this.state.count})</Header>
+                        <Accordion>
+                            <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
+                                <Icon name='dropdown' />
+                                User List
+                            </Accordion.Title>
+                            <Accordion.Content active={activeIndex === 0}>
+                                <Table celled>
+                                    <Table.Header>
+                                        <Table.Row>
+                                            <Table.HeaderCell>ID</Table.HeaderCell>
+                                            <Table.HeaderCell>User Name</Table.HeaderCell>
+                                            <Table.HeaderCell>Platform</Table.HeaderCell>
+                                            <Table.HeaderCell>Locale</Table.HeaderCell>
+                                            <Table.HeaderCell>Badges</Table.HeaderCell>
+                                        </Table.Row>
+                                    </Table.Header>
+                                    <Table.Body>
+                                        {this.state.users}
+                                    </Table.Body>
+                                </Table>
+                            </Accordion.Content>
+
+                        </Accordion>
+
 
                     </Container>
                 </div>
